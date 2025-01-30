7f7230aec91db66e2b730ecf2dfe552f5dad2eb2
yakov6091 <47696805+yakov6091@users.noreply.github.com>
2025-01-30 15:51:06 +0200

    Add files via upload

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,14 +1,15 @@
 'use strict'
-const WALL = ''
-const CELL = '🔲'
 const FLAG = '🚩'
 const BOMB = '💣'
 const HAPPY = '😇'
 const SAD = '😖'
 const WIN = '😎'
 const HINT = '💡'
+const LIVE = '💗'
 
 var gBoard
+var gButton
+var gMines = 2
 
 var gLevel = {
     size: 4,
@@ -16,6 +17,7 @@ var gLevel = {
 }
 
 var gGame = {
+
     isOn: false, //true when game is on
     coveredCount: 0, // how many cells are covered
     markedCount: 0, // how many cells are marked (with flag)
@@ -23,11 +25,15 @@ var gGame = {
 }
 
 function onInit() {
-
+    document.querySelector('.modal').style.display = 'none'
+    gButton = document.querySelector('.face')
+    gButton.innerHTML = HAPPY
     gBoard = buildBoard()
     renderBoard(gBoard)
     setMinesNegsCount(gBoard)
     console.log(gBoard)
+
+    randomMines()
 
 
 }
@@ -41,20 +47,87 @@ function buildBoard() {
         board.push([])
         for (var j = 0; j < size; j++) {
             board[i][j] = {
-                minesAroundCount: 4,
+                minesAroundCount: 0,
                 isCovered: true,
                 isMine: false,
                 isMarked: false,
             }
         }
     }
-
-    board[1][2] = BOMB
-
-    board[3][1] = BOMB
+    // board[1][2].isMine = true
+    // board[3][1].isMine = true
     return board
 }
 
+function renderBoard(mat) {
+    var strHTML = `<table><tbody>`
+    for (var i = 0; i < mat.length; i++) {
+        strHTML += `<tr>`
+        for (var j = 0; j < mat[0].length; j++) {
+            // const cell = mat[i][j]
+
+            const className = `cell cell-${i}-${j}`
+            strHTML += `<td class ="${className}"
+                       onclick = "onCellClicked(this,${i},${j})">
+                       
+                       </td>`
+        }
+        strHTML += `</tr>`
+    }
+    var elTable = document.querySelector('.board')
+    elTable.innerHTML = strHTML
+}
+
+//TODO: if is bomb or cell
+function onCellClicked(elCell, i, j) {
+    var cell = gBoard[i][j]
+    if (cell.isMine) {
+        // elCell.style.backgroundColor = 'red'
+        // elCell.innerHTML = BOMB
+        revealMines()
+        document.querySelector('.modal').style.display = 'block'
+        gButton = document.querySelector('.face')
+        gButton.innerHTML = SAD
+        return
+
+    }
+    // change the if
+    if (cell.isCovered) {
+        cell.isCovered = false
+        elCell.innerText = cell.minesAroundCount
+        elCell.style.backgroundColor = 'grey'
+    }
+    renderAmountOfMines()
+}
+
+function getRandomPos() {
+    var randomRowIdx = getRandomInt(1, gBoard.length - 1)
+    var randomColIdx = getRandomInt(1, gBoard[0].length - 1)
+    return {
+        i: randomRowIdx,
+        j: randomColIdx
+    }
+}
+
+//FIX duplicate location
+function randomMines() {
+    for (var i = 0; i < gMines; i++) {
+        var randomCellPos = getRandomPos() // {i:2, j:3}
+
+        if (gBoard[randomCellPos.i][randomCellPos.j].isMine === true) continue
+
+        else if (!gBoard[randomCellPos.i][randomCellPos.j].isMine) {
+            gBoard[randomCellPos.i][randomCellPos.j].isMine = true
+        }
+    }
+    renderBoard(gBoard)
+    setMinesNegsCount(gBoard)
+}
+
+function renderAmountOfMines() {
+    var elMines = document.querySelector('h2 span')
+    elMines.innerText = gMines
+}
 
 function setMinesNegsCount(board) {
     for (var i = 0; i < board.length; i++) {
@@ -77,13 +150,14 @@ function countMinesAround(cellI, cellJ, board) {
     return neighborsMines
 }
 
-
 function revealMines() {
     for (var i = 0; i < gBoard.length; i++) {
-        for (var j = 0; i < gBoard[0].length; j++) {
+        for (var j = 0; j < gBoard[0].length; j++) {
             var tile = gBoard[i][j]
-            if (tile === BOMB) {
-
+            if (tile.isMine) {
+                var elCell = document.querySelector(`.cell-${i}-${j}`)
+                elCell.innerHTML = BOMB
+                elCell.style.backgroundColor = 'red'
             }
         }
     }
--- utils.js
+++ utils.js
@@ -1,28 +1,5 @@
 'use strict'
 
-function renderBoard(mat) {
-    var strHTML = `<table><tbody>`
-    for (var i = 0; i < mat.length; i++) {
-        strHTML += `<tr>`
-        for (var j = 0; j < mat[0].length; j++) {
-            // const cell = mat[i][j]
-            const className = `cell cell-${i}-${j}`
-            strHTML += `<td class ="${className}"
-                       onclick = "onCellClicked(${i},${j})">${CELL} </button>
-                       </td>`
-        }
-        strHTML += `</tr>`
-    }
-    var elTable = document.querySelector('.board')
-    elTable.innerHTML = strHTML
-}
-
-
-function onCellClicked(elCell, i, j) {
-    var currCell = ''
-
-
-}
 
 
 
